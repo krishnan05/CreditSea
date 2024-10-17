14607469e7c62ee80cfc6b69f0d7849db74ec77e
krishnan05 <aayushkr@iitbhilai.ac.in>
2024-10-17 14:49:43 +0530

    db connection and config and routes

---
+++ server/src/routes/userRoute.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { 
+   
+} from "../controllers/userControllers";
+
+// Create a router instance
+const router: Router = Router();
+
+// Define routes with typed parameters
+router.post("/register", );
+
+// Export the router
+export default router;
